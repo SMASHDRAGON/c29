74840632476a7710966f016b6bca9834260f9105
SMASHDRAGON <manas.chauhan372@gmail.com>
2020-09-16 16:29:36 +0530

    Image

--- Slingshot.js
+++ Slingshot.js
@@ -6,7 +6,11 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+this.sling1=loadImage ("sprites/sling1.png");
+this.sling2=loadImage("sprites/sling2.png");
+this.sling3=loadImage("sprites/sling3.png");
+
+this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
@@ -16,11 +20,17 @@ class SlingShot{
     }
 
     display(){
+image(this.sling1,200,20);
+image (this.sling2,170,20);
+
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            strokeWeight(8);
+           line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+           line(pointA.x,pointA.y,pointB.x+20,pointB.y)
+            image (this.sling3,pointA.x-20,pointA.y,15,30);
         }
     }
     
